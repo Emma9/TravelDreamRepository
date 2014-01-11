6928f541f4eb5c2f35a9e7a895a03d8a3a713810
emma.balgera@gmail.com <emma.balgera@gmail.com@fefa4510-7f2a-1a16-1985-ec03924cecef>
2014-01-11 13:48:12 +0000

    servlet

--- trunk/TravelDreamEJBClient/ejbModule/it/polimi/traveldream/ejb/ClienteBeanLocal.java
+++ trunk/TravelDreamEJBClient/ejbModule/it/polimi/traveldream/ejb/ClienteBeanLocal.java
@@ -1,9 +1,14 @@
 package it.polimi.traveldream.ejb;
 
+import it.polimi.traveldream.entities.PacchettoPersonalizzato;
+
+import java.util.ArrayList;
+
 import javax.ejb.Local;
 
 @Local
 public interface ClienteBeanLocal {
+	
 
 	
 	
