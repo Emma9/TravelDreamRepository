f2916477f556fe9f9e494d2e82f1fa6f902aae18
emma.balgera@gmail.com <emma.balgera@gmail.com@fefa4510-7f2a-1a16-1985-ec03924cecef>
2014-01-13 15:00:33 +0000

    servlet

--- trunk/TravelDreamWeb/src/accessServlet/RegistrationServlet.java
+++ trunk/TravelDreamWeb/src/accessServlet/RegistrationServlet.java
@@ -1,11 +1,24 @@
 package accessServlet;
 
+import it.polimi.traveldream.ejb.ClienteBeanRemote;
+import it.polimi.traveldream.entities.PacchettoPersonalizzato;
+
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Hashtable;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
 
 /**
  * Servlet implementation class RegistrationServlet
@@ -33,7 +46,58 @@ public class RegistrationServlet extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		PrintWriter out= response.getWriter();
+		try{
+			  Context ctx = getInitialContext();
+              request.getSession().setAttribute("context", ctx);
+             
+              ClienteBeanRemote clienteRemoto= (ClienteBeanRemote) ctx
+  					.lookup("ClienteBean/remote");
+             
+              String nome= request.getParameter("nome");
+              String cognome= request.getParameter("cognome");
+              String codiceFiscale= request.getParameter("codiceFiscale");
+              String email= request.getParameter("email");
+              String password= request.getParameter("password");
+
+              if(nome.equals("")||cognome.equals("")||email.equals("")||password.length()<8){
+                  response.sendRedirect("registrazione.xhtml");
+              }else{
+            	  long id = clienteRemoto.createCliente(email, password, codiceFiscale, nome, cognome);
+            	  
+            	  if(id==-1){
+            		RequestDispatcher rd= ((ServletRequest) ctx).getRequestDispatcher("registrazione.xhtml");
+      				out.println("<p>I dati inseriti non sono corretti </p>");
+      				rd.include(request, response);
+      				
+            	  }else{
+            		  request.getSession().setAttribute("idCliente", clienteRemoto.findByIdCliente(id).getIdCliente());
+            		  request.getSession().setAttribute("Cliente", clienteRemoto.findByIdCliente(id));
+            		  ArrayList<PacchettoPersonalizzato> elencoPacchettiCliente = new ArrayList<PacchettoPersonalizzato>();
+      				
+                      
+                      request.getSession().setAttribute("elencoPacchettiCliente", elencoPacchettiCliente);
+                     
+                      response.sendRedirect("homepageCliente.xhtml");
+
+            	  }
+              }
+              
+              
+		}catch(NamingException e){
+			//da implementare!!!!!!!!!!!!!!
+		}
+		
 		// TODO Auto-generated method stub
+	}
+
+	private Context getInitialContext() throws NamingException {
+		// TODO Auto-generated method stub
+		Hashtable<String,String> env = new Hashtable<String,String>();
+        env.put(Context.INITIAL_CONTEXT_FACTORY, "org.jnp.interfaces.NamingContextFactory");
+        env.put(Context.PROVIDER_URL,"localhost:1099");
+        return new InitialContext(env);
 	}
 
 }
