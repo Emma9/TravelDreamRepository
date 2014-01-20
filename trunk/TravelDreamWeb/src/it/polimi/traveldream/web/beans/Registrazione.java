caf4248f4be5c11718bead75bd4a7bb3ab6a787a
emma.balgera@gmail.com <emma.balgera@gmail.com@fefa4510-7f2a-1a16-1985-ec03924cecef>
2014-01-20 20:23:56 +0000

    beans

--- trunk/TravelDreamWeb/src/it/polimi/traveldream/web/beans/Registrazione.java
+++ trunk/TravelDreamWeb/src/it/polimi/traveldream/web/beans/Registrazione.java
@@ -2,4 +2,7 @@ package it.polimi.traveldream.web.beans;
 
 public interface Registrazione {
 
+	void inserisciNelDatabase(String nome, String cognome,
+			String codiceFiscale, String email, String password);
+
 }
