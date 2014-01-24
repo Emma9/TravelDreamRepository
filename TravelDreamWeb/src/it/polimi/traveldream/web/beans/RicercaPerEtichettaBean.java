532ce2b43223dc258c3232386bca2fabfa1f1d72
ipbnem@gmail.com <ipbnem@gmail.com@fefa4510-7f2a-1a16-1985-ec03924cecef>
2014-01-24 09:17:58 +0000

    ricerca per etichetta

--- TravelDreamWeb/src/it/polimi/traveldream/web/beans/RicercaPerEtichettaBean.java
+++ TravelDreamWeb/src/it/polimi/traveldream/web/beans/RicercaPerEtichettaBean.java
@@ -1,13 +1,18 @@
 package it.polimi.traveldream.web.beans;
 
 import it.polimi.traveldream.ejb.client.PacchettoBeanRemote;
+import it.polimi.traveldream.entities.EtichettaDTO;
+import it.polimi.traveldream.entities.PacchettoDTO;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
 
 import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+import javax.servlet.http.HttpServletRequest;
 
 @ManagedBean()
 @SessionScoped
@@ -21,9 +26,149 @@ public class RicercaPerEtichettaBean implements Serializable {
 	@EJB
 	private PacchettoBeanRemote pacchettoRemoto;
 	
-//	private Etichetta etichetta;
+	private EtichettaDTO etichetta;
 	private Date dataPartenza;
 	private Date dataArrivo;
 	private int numPartecipanti;
+	
+	//PACCHETTO SELEZIONATO
+		private PacchettoDTO pacchettoSelezionato;
+		
+		//LISTA PACCHETTI INVIATA ALLA PAGINA WEB
+		private ArrayList<PacchettoDTO> pacchettiRicercati = new ArrayList<PacchettoDTO>();
+	
+	
+	/**
+	 * @return the etichetta
+	 */
+	public EtichettaDTO getEtichetta() {
+		return etichetta;
+	}
+	/**
+	 * @param etichetta the etichetta to set
+	 */
+	public void setEtichetta(EtichettaDTO etichetta) {
+		this.etichetta = etichetta;
+	}
+	/**
+	 * @return the dataPartenza
+	 */
+	public Date getDataPartenza() {
+		return dataPartenza;
+	}
+	/**
+	 * @param dataPartenza the dataPartenza to set
+	 */
+	public void setDataPartenza(Date dataPartenza) {
+		this.dataPartenza = dataPartenza;
+	}
+	/**
+	 * @return the dataArrivo
+	 */
+	public Date getDataArrivo() {
+		return dataArrivo;
+	}
+	/**
+	 * @param dataArrivo the dataArrivo to set
+	 */
+	public void setDataArrivo(Date dataArrivo) {
+		this.dataArrivo = dataArrivo;
+	}
+	/**
+	 * @return the numPartecipanti
+	 */
+	public int getNumPartecipanti() {
+		return numPartecipanti;
+	}
+	/**
+	 * @param numPartecipanti the numPartecipanti to set
+	 */
+	public void setNumPartecipanti(int numPartecipanti) {
+		this.numPartecipanti = numPartecipanti;
+	}
 
+	/**
+	 * @return the pacchettoSelezionato
+	 */
+	public PacchettoDTO getPacchettoSelezionato() {
+		return pacchettoSelezionato;
+	}
+	/**
+	 * @param pacchettoSelezionato the pacchettoSelezionato to set
+	 */
+	public void setPacchettoSelezionato(PacchettoDTO pacchettoSelezionato) {
+		this.pacchettoSelezionato = pacchettoSelezionato;
+	}
+	/**
+	 * @return the pacchettiRicercati
+	 */
+	public ArrayList<PacchettoDTO> getPacchettiRicercati() {
+		return pacchettiRicercati;
+	}
+	/**
+	 * @param pacchettiRicercati the pacchettiRicercati to set
+	 */
+	public void setPacchettiRicercati(ArrayList<PacchettoDTO> pacchettiRicercati) {
+		this.pacchettiRicercati = pacchettiRicercati;
+	}
+	
+	
+	
+	public void ricercaPerEtichetta(){
+		
+		FacesContext context = FacesContext.getCurrentInstance();
+		HttpServletRequest request = (HttpServletRequest) context
+				.getExternalContext().getRequest();
+		
+		ArrayList<PacchettoDTO> pacchetti = new ArrayList<PacchettoDTO>();
+		
+		//try {
+		
+		
+		if(this.pacchettoRemoto.verificaConsistenzaDate(this.dataPartenza, this.dataArrivo)){
+			//LE DATE INSERITE SONO VALIDE
+		
+		pacchetti = pacchettoRemoto.ricercaPerEtichetta(this.etichetta, this.dataPartenza, this.dataArrivo);
+			//RITORNA LA LISTA DEI PACCHETTI CON DESTINAZIONE DESIDERATA E DISPONIBILI NEL PERIODO RICHIESTO
+		
+		for(int i=0;i<=pacchetti.size();i++){
+			//PER OGNI PACCHETTO VERIFICA CHE TUTTI I SUOI COMPONENTI SIANO DISPONIBILI
+			
+			if(pacchettoRemoto.verificaDisponibilitaComponenti(this.dataPartenza, this.dataArrivo, this.numPartecipanti, pacchetti.get(i).getListaComponenti())){
+			//SE TUTTI I COMPONENTI SONO DISPONIBILI NON RIMUOVE IL PACCHETTO DALLA LISTA
+				
+			}else{
+				pacchetti.set(i, null);
+			//SE CI SONO DEI COMPONENTI NON DISPONIBILI ANNULLA IL PACCHETTO CHE LI CONTIENE	
+				
+			}
+			
+		}				
+		
+		for(int j=0;j<=pacchetti.size();j++){
+			//SCORRE LA LISTA DEI PACCHETTI
+		
+		if(pacchetti.get(j)==null){
+			//SE PACCHETTO == NULL PASSA AL PROSSIMO PACCHETTO
+			
+		}else{
+			//SE PACCHETTO != NULL LO COPIA IN PACCHETTIRICERCATI
+			pacchettiRicercati.set(j, pacchetti.get(j));
+		}
+			
+		}
+			
+		}
+		
+
+	/*} catch (ServletException e) {
+		
+		return null;
+
+	}*/
+	
+	//return pacchettiRicercati;
+		
+	}
+	
 }
