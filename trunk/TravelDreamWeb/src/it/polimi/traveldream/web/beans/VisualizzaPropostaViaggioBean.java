66313124cf091930a74460f00257a5014eaebb7b
biancinisara12@gmail.com <biancinisara12@gmail.com@fefa4510-7f2a-1a16-1985-ec03924cecef>
2014-01-31 08:35:14 +0000

    aggiunte beans

---
+++ trunk/TravelDreamWeb/src/it/polimi/traveldream/web/beans/VisualizzaPropostaViaggioBean.java
@@ -0,0 +1,17 @@
+package it.polimi.traveldream.web.beans;
+
+import java.io.Serializable;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+@ManagedBean()
+@SessionScoped
+public class VisualizzaPropostaViaggioBean implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 5326L;
+
+}
