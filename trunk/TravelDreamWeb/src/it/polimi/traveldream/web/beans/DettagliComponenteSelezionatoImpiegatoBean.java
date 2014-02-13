6f5aab9bcd9090cfbddcfc81d60979b0138d635e
ipbnem@gmail.com <ipbnem@gmail.com@fefa4510-7f2a-1a16-1985-ec03924cecef>
2014-02-13 14:15:59 +0000

    impiegato

--- trunk/TravelDreamWeb/src/it/polimi/traveldream/web/beans/DettagliComponenteSelezionatoImpiegatoBean.java
+++ trunk/TravelDreamWeb/src/it/polimi/traveldream/web/beans/DettagliComponenteSelezionatoImpiegatoBean.java
@@ -13,7 +13,7 @@ import javax.faces.bean.SessionScoped;
 
 @ManagedBean()
 @SessionScoped
-public class DettagliComponenteSelezionatoBean implements Serializable {
+public class DettagliComponenteSelezionatoImpiegatoBean implements Serializable {
 
 	/**
 	 * 
